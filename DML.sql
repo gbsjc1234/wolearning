f22adfa2a2ae70d46a596c634b8aeadc0e6a38b7
kyzgsjc1234 <1484868182@qq.com>
2018-04-29 22:48:24 +0800

    add init data

---
+++ DML.sql
@@ -0,0 +1,20 @@
+INSERT INTO user_account (refid ,
+  create_time,
+  create_refid,
+  name ,
+  role ,
+  password ,
+  nick_name ,
+  authority
+)
+values
+(
+'4331babb52ad4cbd-9477befad197ee5a',
+'2018-04-29 22:27:00',
+'DFADS',
+'admin',
+'admin',
+'21232f297a57a5a743894a0e4a801fc3',
+'管理员',
+'admin'
+)
