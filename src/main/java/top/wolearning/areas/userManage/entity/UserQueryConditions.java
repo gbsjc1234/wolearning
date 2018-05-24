dfb120f7cb8635c950a70be333e70c8b7c1054b5
jp <1257196064@qq.com>
2018-05-24 17:29:46 +0800

    userManager模块

---
+++ src/main/java/top/wolearning/areas/userManage/entity/UserQueryConditions.java
@@ -0,0 +1,65 @@
+package top.wolearning.areas.userManage.entity;
+
+import java.util.Date;
+
+public class UserQueryConditions {
+    private String name;
+
+    private Date start;
+
+    private Date end;
+
+    private Integer isDeleted;
+
+    private Integer pageIndex;
+
+    private Integer pageSize;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public Integer getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Integer isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public Integer getPageIndex() {
+        return pageIndex;
+    }
+
+    public void setPageIndex(Integer pageIndex) {
+        this.pageIndex = pageIndex;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+}
